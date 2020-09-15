e155d4f4392d6a6f5392fa36c13bba669c059e64
Juan Monetta <jpmonettas@gmail.com>
2020-09-15 17:41:59 -0300

    Giving the ui some love

--- styles-src/flow_storm_debugger/styles/main.clj
+++ styles-src/flow_storm_debugger/styles/main.clj
@@ -1,12 +1,22 @@
 (ns flow-storm-debugger.styles.main)
 
+(def theme
+  {:code-font "#eaeaea"
+   :background "#292c34"
+   :current-expr "#902638"
+   :tab-background "#394e68"
+   :link-color :pink})
 (def ^{:garden {:output-to "resources/public/css/main.css"}}
   main
   (list
    [:html
+    [:a {:color (:link-color theme)}]
+    [:button {:cursor :pointer}]
     [:body {:height "100vh"
             :padding 0
-            :margin 0}
+            :margin 0
+            :background-color (:background theme)
+            :color "#fcfcfc"}
      [:#app {:height "100%"}
       [:.main-screen {:height "90%"}
 
@@ -14,6 +24,7 @@
                     :margin "100px"}]
 
        [:.panel {:padding "10px"
+                 :margin "5px"
                  :overflow :scroll
                  :border "1px solid #aaa"}
         [:&.controls {:overflow :hidden}]]
@@ -23,10 +34,9 @@
                :display :inline-block
                :text-align :right
                :min-width "50px"
-               :border-radius "0px 5px 0px 0px"}
-        [:&.active {:background-color "#bbb"}]
-        [:.name {:font-size "12px"
-                 :color "#444"}]
+               :cursor :pointer}
+        [:&.active {:background-color (:tab-background theme)}]
+        [:.name {:font-size "12px"}]
         [:.close {:font-size "10px"
                   :margin-left "4px"
                   :padding-left "4px"
@@ -38,14 +48,15 @@
         [:.selected-flow {:height "100%"
                           :padding "5px"}
          [:.trace-count {:margin-left "10px"}]
-         [:.flow-code-result {:height "75%"
+         [:.flow-code-result {:height "95%"
                               :display :flex}
 
           [:.code {:width "50%"
                    :display :inline-block
                    :min-width "450px"
                    :height "90%"}
-           [:.hl {:background-color :pink}]]
+           [:.hl {:background-color (:current-expr theme)
+                  :border-radius "5px"}]]
 
           [:.result {:width "50%"
                      :display :inline-block
