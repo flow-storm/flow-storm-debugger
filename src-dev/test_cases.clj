26e418f75aa2b2450853b08b7bcd4a4a304cfc59
Juan Pedro Monetta Sanchez <jmonetta@debianLaptop>
2022-12-15 14:46:35 -0300

    Wrap test_caes.clj in comment so they don't execute on refresh

--- src-dev/test_cases.clj
+++ src-dev/test_cases.clj
@@ -4,88 +4,91 @@
              [clojure.set :as set]
              [dev-tester]))
 
-(fs-api/local-connect)
+(comment
+
+  (fs-api/local-connect)
 
 ;;;;;;;;;;;;;;;;;;;
-;; Basic #rtrace ;;
+  ;; Basic #rtrace ;;
 ;;;;;;;;;;;;;;;;;;;
 
-#rtrace (reduce + (map inc (range 10)))
+  #rtrace (reduce + (map inc (range 10)))
 
 ;;;;;;;;;;;;;;;;;;
-;; Basic #trace ;;
+  ;; Basic #trace ;;
 ;;;;;;;;;;;;;;;;;;
 
-#trace
-(defn factorial [n]
-  (if (zero? n)
-    1
-    (* n (factorial (dec n)))))
+  #trace
+  (defn factorial [n]
+    (if (zero? n)
+      1
+      (* n (factorial (dec n)))))
 
-(factorial 5)
+  (factorial 5)
 
 ;;;;;;;;;;;;;
-;; Def val ;;
+  ;; Def val ;;
 ;;;;;;;;;;;;;
 
-user/v
+  user/v
 
 ;;;;;;;;;;;;;;;;;
-;; Inspect val ;;
+  ;; Inspect val ;;
 ;;;;;;;;;;;;;;;;;
 
-(tap> (all-ns))
+  (tap> (all-ns))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; REPL [un]instrument var ;;
+  ;; REPL [un]instrument var ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(fs-api/instrument-var-clj 'clojure.set/difference)
+  (fs-api/instrument-var-clj 'clojure.set/difference)
 
-(set/difference #{1 2 3} #{2})
+  (set/difference #{1 2 3} #{2})
 
-(fs-api/uninstrument-var-clj 'clojure.set/difference)
+  (fs-api/uninstrument-var-clj 'clojure.set/difference)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; REPL [un]instrument namespaces ;;
+  ;; REPL [un]instrument namespaces ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(fs-api/instrument-namespaces-clj #{"dev-tester"})
+  (fs-api/instrument-namespaces-clj #{"dev-tester"})
 
-(dev-tester/boo [1 "hello" 4])
+  (dev-tester/boo [1 "hello" 4])
 
-(fs-api/uninstrument-namespaces-clj #{"dev-tester"})
+  (fs-api/uninstrument-namespaces-clj #{"dev-tester"})
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; - Cli-run                         ;;
-;; - Re-run flow                     ;;
-;; - Call tree                       ;;
-;; - Call tree goto trace            ;;
-;; - Call search                     ;;
-;; - Functions list                  ;;
-;; - Functions list goto trace       ;;
-;; - Fully instrument form           ;;
-;; - UnInstrument forms from fn list ;;
+  ;; - Cli-run                         ;;
+  ;; - Re-run flow                     ;;
+  ;; - Call tree                       ;;
+  ;; - Call tree goto trace            ;;
+  ;; - Call search                     ;;
+  ;; - Functions list                  ;;
+  ;; - Functions list goto trace       ;;
+  ;; - Fully instrument form           ;;
+  ;; - UnInstrument forms from fn list ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-make test-instrument-own-cljs-light
+  make test-instrument-own-cljs-light
 
 ;;;;;;;;;;
-;; Taps ;;
+  ;; Taps ;;
 ;;;;;;;;;;
 
-(tap> (all-ns))
+  (tap> (all-ns))
 
 ;;;;;;;;;;;;
-;; Themes ;;
+  ;; Themes ;;
 ;;;;;;;;;;;;
 
-(fs-api/stop)
+  (fs-api/stop)
 
-(fs-api/local-connect {:theme :dark})
+  (fs-api/local-connect {:theme :dark})
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; For remote debugging testing ;;
+  ;; For remote debugging testing ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Login on windows vm
+  ;; Login on windows vm
+  )
