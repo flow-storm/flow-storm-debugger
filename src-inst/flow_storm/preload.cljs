cd444358447a6ed8b7983b7804e62040149f6b97
Juan Monetta <jpmonettas@gmail.com>
2023-09-04 10:16:04 -0300

    Setup ClojureScript storm tracers in preload

--- src-inst/flow_storm/preload.cljs
+++ src-inst/flow_storm/preload.cljs
@@ -1,4 +1,12 @@
 (ns flow-storm.preload
-  (:require [flow-storm.api :as fs-api]))
+  (:require [flow-storm.api :as fs-api]
+            [flow-storm.tracer :as tracer]))
+
+;; TODO: make this conditional
+(js* "cljs.storm.tracer.trace_expr_fn=flow_storm.tracer.trace_expr_exec;")
+(js* "cljs.storm.tracer.trace_fn_call_fn=flow_storm.tracer.trace_fn_call;")
+(js* "cljs.storm.tracer.trace_fn_return_fn=flow_storm.tracer.trace_fn_return;")
+(js* "cljs.storm.tracer.trace_bind_fn=flow_storm.tracer.trace_bind;")
+(js* "cljs.storm.tracer.trace_form_init_fn=flow_storm.tracer.trace_form_init;")
 
 (fs-api/remote-connect {})
