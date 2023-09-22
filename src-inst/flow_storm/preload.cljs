824f60a905338d4f14f90d585d5b2b490b0d3a47
Juan Monetta <jpmonettas@gmail.com>
2023-09-22 12:12:01 -0300

    Improve preload.cljs

--- src-inst/flow_storm/preload.cljs
+++ src-inst/flow_storm/preload.cljs
@@ -1,12 +1,13 @@
 (ns flow-storm.preload
-  (:require [flow-storm.api :as fs-api]
-            [flow-storm.tracer :as tracer]))
+  (:require [flow-storm.api :as fs-api]))
 
-;; TODO: make this conditional
-(js* "cljs.storm.tracer.trace_expr_fn=flow_storm.tracer.trace_expr_exec;")
-(js* "cljs.storm.tracer.trace_fn_call_fn=flow_storm.tracer.trace_fn_call;")
-(js* "cljs.storm.tracer.trace_fn_return_fn=flow_storm.tracer.trace_fn_return;")
-(js* "cljs.storm.tracer.trace_bind_fn=flow_storm.tracer.trace_bind;")
-(js* "cljs.storm.tracer.trace_form_init_fn=flow_storm.tracer.trace_form_init;")
+(js* "try {
+         cljs.storm.tracer.trace_expr_fn=flow_storm.tracer.trace_expr_exec;
+         cljs.storm.tracer.trace_fn_call_fn=flow_storm.tracer.trace_fn_call;
+         cljs.storm.tracer.trace_fn_return_fn=flow_storm.tracer.trace_fn_return;
+         cljs.storm.tracer.trace_bind_fn=flow_storm.tracer.trace_bind;
+         cljs.storm.tracer.trace_form_init_fn=flow_storm.tracer.trace_form_init;
+         console.log(\"ClojureScriptStorm functions plugged in.\");
+       } catch (error) {console.log(\"ClojureScriptStorm not detected.\")}")
 
 (fs-api/remote-connect {})
