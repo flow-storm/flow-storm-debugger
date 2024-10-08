f08fa3ec66944e56931671efb9ccb76deee1b57c
Juan Monetta <jpmonettas@gmail.com>
2024-10-08 14:43:35 -0300

    Adding data-window-push-val and data-window-val-update to cljs

--- src-inst/flow_storm/api.cljs
+++ src-inst/flow_storm/api.cljs
@@ -36,3 +36,14 @@
 
   [limit]
   (dbg-api/set-thread-trace-limit limit))
+
+(defn data-window-push-val
+  ([dw-id val] (data-window-push-val dw-id val nil))
+  ([dw-id val stack-key]
+   (let [vdata (assoc (rt-values/extract-data-aspects val)
+                      :flow-storm.debugger.ui.data-windows.data-windows/dw-id dw-id
+                      :flow-storm.debugger.ui.data-windows.data-windows/stack-key stack-key)]
+     (rt-events/publish-event! (rt-events/make-data-window-push-val-data-event dw-id vdata)))))
+
+(defn data-window-val-update [dw-id new-val]
+  (rt-events/publish-event! (rt-events/make-data-window-update-event dw-id {:new-val new-val})))
