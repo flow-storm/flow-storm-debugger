871e55407daa4b5d6c2d99ac81e2018a491aba60
Juan Monetta <jpmonettas@gmail.com>
2025-03-11 09:58:34 -0300

    Releasing 4.2.1

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,5 @@
-((clojure-mode . ((cider-clojure-cli-aliases . "dev:storm"))))
+((clojure-mode . ((cider-clojure-cli-aliases . "dev:dev-tools:storm")
+				  (clojure-dev-menu-name . "flow-storm-dev-menu")                  
+                  (cider-jack-in-nrepl-middlewares . ("refactor-nrepl.middleware/wrap-refactor"
+                                                      "cider.nrepl/cider-middleware"
+                                                      "flow-storm.nrepl.middleware/wrap-flow-storm")))))
