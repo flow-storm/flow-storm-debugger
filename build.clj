76fcfa24c8645331af95a4233ff4fc5d318bc2a8
Juan Monetta <jpmonettas@gmail.com>
2025-05-21 10:27:31 -0300

    Release 4.4.3

--- build.clj
+++ build.clj
@@ -5,7 +5,7 @@
             [clojure.spec.alpha :as s]))
 
 (def version (or (System/getenv "VERSION")
-                 "4.4.2"))
+                 "4.4.3"))
 
 (def target-dir "target")
 (def class-dir (str target-dir "/classes"))
