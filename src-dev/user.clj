dbba58043c38f4c948cd1399a0af0c5cd820f194
Juan Monetta <jpmonettas@gmail.com>
2026-01-24 18:31:20 -0300

    We don't need the Platform/startup hack for cider anymore
    https://github.com/clojure-emacs/orchard/issues/250#issuecomment-3794720426

--- src-dev/user.clj
+++
@@ -1,7 +0,0 @@
-(ns user
-  (:import [javafx.application Platform]
-           [com.sun.javafx.logging PlatformLogger$Level]
-           [com.sun.javafx.util Logging])
-  (:require [flow-storm.utils :refer [log]]))
-
-(Platform/startup (fn [] (log "JavaFX toolkit initialized")))
